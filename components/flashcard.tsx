d8c4455f26382cfedc9b94f438d6c5bc98646a56
Utsargo Roy <royutsargo@gmail.com>
2024-08-20 20:02:10 +0600

    Edit modal

--- components/flashcard.tsx
+++ components/flashcard.tsx
@@ -108,7 +108,10 @@ export default function Flashcard() {
   return (
     <>
       {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+          onClick={() => setIsModalOpen(false)}
+        >
           <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-11/12 md:w-1/3">
             <h2 className="text-xl font-bold mb-4 dark:text-white">
               Wordlist Options
